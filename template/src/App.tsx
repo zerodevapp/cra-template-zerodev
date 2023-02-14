fa857820e9befb4e6fb04a3c06ffd2ef3f92029d
Zobeir Hamid <zobeirhamid@berkeley.edu>
2023-02-14 11:26:16 -0800

    Initial commit

---
+++ template/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { ConnectButton } from 'zerokit';
+
+function App() {
+  return (
+    <div className="App">
+      <ConnectButton />
+    </div>
+  );
+}
+
+export default App;
